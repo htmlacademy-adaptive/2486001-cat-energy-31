9fa8820e18f1d69cb06b96b80b6c24e7c44fee9a
L1ghtManch4k <156451172+L1ghtManch4k@users.noreply.github.com>
2024-06-05 01:43:43 +0600

    первая попытка

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.nav');
+const navToggle = document.querySelector('.nav__toggle');
+
+navMain.classList.remove('nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('nav--closed')) {
+    navMain.classList.remove('nav--closed');
+    navMain.classList.add('nav--open');
+  } else {
+    navMain.classList.add('nav--closed');
+    navMain.classList.remove('nav--open');
+  }
+});
